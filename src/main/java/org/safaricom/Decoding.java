59304f7bb62c95e3c338b8ad8591dd0fb172e763
Clara-Musyoka <musyokamusenya@gmail.com>
2023-07-01 16:51:49 +0300

    Add the encoding Test

--- src/main/java/org/safaricom/Decoding.java
+++ src/main/java/org/safaricom/Decoding.java
@@ -1,21 +1,27 @@
 package org.safaricom;
 
 public class Decoding {
-    //to keep track of index
+    // to keep track of index
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
     public static String decode(String cipherText, int shiftKey) {
         cipherText = cipherText.toLowerCase();
-        String message = "";
+        StringBuilder message = new StringBuilder();
         for (int ii = 0; ii < cipherText.length(); ii++) {
-            int charPosition = ALPHABET.indexOf(cipherText.charAt(ii));
+            char currentChar = cipherText.charAt(ii);
+            if (!Character.isLetter(currentChar)) {
+                // Ignore non-alphabetic characters
+                message.append(currentChar);
+                continue;
+            }
+            int charPosition = ALPHABET.indexOf(currentChar);
             int keyVal = (charPosition - shiftKey) % 26;
             if (keyVal < 0) {
                 keyVal = ALPHABET.length() + keyVal;
             }
             char replaceVal = ALPHABET.charAt(keyVal);
-            message += replaceVal;
+            message.append(replaceVal);
         }
-        return message;
+        return message.toString();
     }
 }
--- src/main/java/org/safaricom/Encoding.java
+++ src/main/java/org/safaricom/Encoding.java
@@ -1,18 +1,23 @@
 package org.safaricom;
-
 public class Encoding {
-    //to keep track of index
+    // to keep track of index
     public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
 
     public static String encode(String message, int shiftKey) {
         message = message.toLowerCase();
-        String cipherText = "";
+        StringBuilder cipherText = new StringBuilder();
         for (int ii = 0; ii < message.length(); ii++) {
-            int charPosition = alpha.indexOf(message.charAt(ii));
+            char currentChar = message.charAt(ii);
+            if (!Character.isLetter(currentChar)) {
+                // Ignore non-alphabetic characters
+                cipherText.append(currentChar);
+                continue;
+            }
+            int charPosition = alpha.indexOf(currentChar);
             int keyVal = (shiftKey + charPosition) % 26;
             char replaceVal = alpha.charAt(keyVal);
-            cipherText += replaceVal;
+            cipherText.append(replaceVal);
         }
-        return cipherText;
+        return cipherText.toString().toUpperCase();
     }
 }
