b930f809b1bf20cbc5d9a531164fd86c71f4fbc1
Clara-Musyoka <musyokamusenya@gmail.com>
2023-07-01 17:07:42 +0300

    check process in the main class

--- src/main/java/org/safaricom/Main.java
+++ src/main/java/org/safaricom/Main.java
@@ -2,38 +2,44 @@ package org.safaricom;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner userInputObject = new Scanner(System.in);
 
         do {
-            System.out.println("Select process (e-encoding/d-decoding)");
-            String process = userInputObject.next();
+            System.out.println("Select process (e-encoding/d-decoding):");
+            String process = userInputObject.next().toLowerCase();
 
+            if (process.startsWith("e")) {
+                System.out.println("Enter Message:");
+                String message = userInputObject.next();
 
-            System.out.println("Enter Message: ");
-            String message = userInputObject.next();
+                System.out.println("Enter Key:");
+                int key = userInputObject.nextInt();
 
-            System.out.println("Enter Key: ");
-            int key = userInputObject.nextInt();
+                System.out.println("Encrypted Message: " + Encoding.encode(message, key));
+            } else if (process.startsWith("d")) {
+                System.out.println("Enter Cipher Text:");
+                String cipherText = userInputObject.next();
 
+                System.out.println("Enter Key:");
+                int key = userInputObject.nextInt();
 
-            //encoding process
-if(process.startsWith("e")){
-    System.out.println("\nEncrpyted msg:" + Encoding.encode(message, key));
-}else if(process.startsWith("d")){
-    System.out.println("\nDecrypted Message:" + Decoding.decode(message, key));
+                System.out.println("Decrypted Message: " + Decoding.decode(cipherText, key));
+            } else {
+                System.out.println("Invalid process. Please try again.");
+                continue;
+            }
 
-}else{
-    System.out.println("invalid process");
-    continue;
-}
-
-            System.out.println("Do you want to Quit or Continue? ");
-            String nextStep = userInputObject.next();
+            System.out.println("Do you want to Quit or Continue?");
+            String nextStep = userInputObject.next().toLowerCase();
             if (nextStep.startsWith("q")) {
                 break;
             }
         } while (true);
+
+        userInputObject.close();
     }
 }
